aec4c997392a5c4136182bda907d21bd00301386
pponnapalli87 <pponnapalli.87@gmail.com>
2021-07-15 00:44:28 -0700

    Created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,7 +1,6 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-
+const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
 
 const devConfig = {
@@ -18,10 +17,7 @@ const devConfig = {
             remotes: {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js'
             },
-            shared: ['react', 'react-dom']
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
+            shared: packageJson.dependencies,
         }),
     ],
 }
